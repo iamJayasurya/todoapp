6faea8b4e3ddf377296e15e6d007c05a8a922de7
iamJayasurya <j.jayasuriya1999@gmail.com>
2024-09-12 20:30:11 +0530

    edited

--- src/Models/Card.js
+++ src/Models/Card.js
@@ -59,13 +59,13 @@ function CARD({taskobj,indexOF,DeleteTask, updateListArray}) {
         <hr style={{borderTop:`3px solid ${colors[indexOF%5].primaryColor}`,opacity:"1"}}></hr>
             </div>
         <div className='button-group'>
-            <a className='completed' onClick={StatusCompletd}>
+            <a href="button" className='completed' onClick={StatusCompletd}>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentcolor" class="bi bi-check-lg" viewBox="0 0 16 16 ">
   <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
 </svg>
 
             </a>
-            <a className='delete' onClick={HandleDelete}>
+            <a  href="button" className='delete' onClick={HandleDelete}>
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentcolor" class="bi bi-x-lg " viewBox="0 1 16 16">
   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
 </svg>
